d650590829ddf7bf175667a4d0e9d3ac0308ade5
Daniil Dorofeev <misterfoxkk@gmail.com>
2025-01-22 21:48:08 +0400

    update user agent

--- headers.js
+++ headers.js
@@ -3,7 +3,7 @@ const token = config.get('token')
 
 const headers = {
     'Cookie': `hhtoken=${token};`,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0',
     'Accept': '*/*',
     'Accept-Encoding': 'gzip, deflate, br'
 }
